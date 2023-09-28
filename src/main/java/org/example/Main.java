7056f734b1a18bb08fafc0114bd2d3a0441110d5
Smokvin <alex.smokvin@gmail.com>
2023-09-28 20:39:50 +0300

    ticket10 done

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] str = {"aa", "bb", "cc", "aa", "bb", "dd"};
+
+        Set<String> stringSet = new HashSet<>(Arrays.asList(str));
+        String result = String.join(" ", stringSet);
+        System.out.println(result);
+    }
+}
